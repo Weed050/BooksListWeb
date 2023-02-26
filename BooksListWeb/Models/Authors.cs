1293ce76a4af80c58939f5615ce5a8ab5863884c
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-02-26 15:22:06 +0100

    created tables for database in code (C#)

---
+++ BooksListWeb/Models/Authors.cs
@@ -0,0 +1,8 @@
+﻿namespace BooksListWeb.Models
+{
+    public class Authors
+    {
+        public int AuthorID { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
---
+++ BooksListWeb/Models/Books.cs
@@ -0,0 +1,9 @@
+﻿namespace BooksListWeb.Models
+{
+    public class Books
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+        public int AuthorID { get; set; }
+    }
+}
